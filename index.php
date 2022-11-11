e8c128e6d617dc7215ee576c2505c9066d2e03b7
trungHacker2k3 <ndtrung30@gmail.com>
2022-11-11 14:50:27 +0700

    udpate

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<h1>
-    hello world
-</h1>
+https://th.bing.com/th/id/OIP.ff0THWKtvzRGZ26eYy8i8wHaE9?w=271&h=181&c=7&r=0&o=5&dpr=1.3&pid=1.7
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</head>
+
+<body>
+
+</body>
+
+</html>
